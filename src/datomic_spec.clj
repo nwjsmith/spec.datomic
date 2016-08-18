cbdad88b2f4d774db9e564c72747b4c6726e7ed5
Nate Smith <nwjsmith@gmail.com>
2016-08-18 10:43:17 -0400

    Remove unused function

--- src/datomic_spec.clj
+++ src/datomic_spec.clj
@@ -3,21 +3,6 @@
             [clojure.string :refer [ends-with? starts-with?]]
             [clojure.test.check.generators :as gen]))
 
-;; From core.incubator
-(defn- dissoc-in
-  "Dissociates an entry from a nested associative structure returning a new
-  nested structure. keys is a sequence of keys. Any empty maps that result
-  will not be present in the new structure."
-  [m [k & ks]]
-  (if ks
-    (if-let [nextmap (get m k)]
-      (let [newmap (dissoc-in nextmap ks)]
-        (if (seq newmap)
-          (assoc m k newmap)
-          (dissoc m k)))
-      m)
-    (dissoc m k)))
-
 (defn prefixed-symbol-spec
   "Takes a prefix and returns a spec for a symbol with that prefix."
   [prefix]
